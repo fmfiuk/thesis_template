1e1ae5393f40d3f5e78cb7b4adc1189cb407010c
Laco Pápay <laco.papay@gmail.com>
2014-04-19 15:40:21 +0200

    Build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode main.tex
+bibtex main
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode main.tex
+xelatex -shell-escape -synctex=1 -interaction=nonstopmode main.tex
